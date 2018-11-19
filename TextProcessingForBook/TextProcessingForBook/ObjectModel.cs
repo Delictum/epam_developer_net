b47c5ca8fe3269742cd40e1a5a34d92e289f7f13
Delictum <31595759+Delictum@users.noreply.github.com>
2018-11-19 12:23:52 +0300

    Create "ObjectModel" class
    1) create new static class for work with xml;
    2) method "CreateObjectModel" transfer to a new class;
    3) add new method for save object model.

---
+++ TextProcessingForBook/TextProcessingForBook/ObjectModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TextProcessingForBook
+{
+    public static class ObjectModel
+    {
+        public static XDocument CreateObjectModel()
+        {
+            XDocument objectModel =
+                new XDocument(
+                    new XElement("TextProcessingForBook", new XAttribute("type", "namespace"),
+                        new XElement("App", new XAttribute("type", "config")),
+                        new XElement("Program", new XAttribute("type", "main")),
+                        new XElement("ObjectModel", new XAttribute("type", "static class")),
+
+                        new XElement("CustomText", new XAttribute("type", "folder"),
+                            new XElement("IText", new XAttribute("type", "interface")),
+                            new XElement("Text", new XAttribute("type", "class")),
+                            new XElement("TextProcessing", new XAttribute("type", "class")),
+
+                            new XElement("TextContent", new XAttribute("type", "folder"),
+                                new XElement("AuxiliaryForSentence", new XAttribute("type", "static class")),
+                                new XElement("Sentence", new XAttribute("type", "class")),
+                                new XElement("SentenceType", new XAttribute("type", "enum")),
+                                new XElement("Word", new XAttribute("type", "class"))),
+
+                                new XElement("Characters", new XAttribute("type", "folder"),
+                                    new XElement("Character", new XAttribute("type", "class")),
+                                    new XElement("CharacterHelper", new XAttribute("type", "static class")),
+                                    new XElement("CharacterType", new XAttribute("type", "enum"))))));
+            return objectModel;
+        }
+
+        public static void SaveObjectModel(XDocument objectModel, string fileName, string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+                filePath = ConfigurationManager.AppSettings["ObjectModel"];
+            else
+            {
+                var builder = new StringBuilder();
+                builder.Append(filePath).Append("\\").Append(fileName).Append(".xml");
+                filePath = builder.ToString();
+            }
+            objectModel.Save(filePath);
+        }
+    }
+}
--- TextProcessingForBook/TextProcessingForBook/Program.cs
+++ TextProcessingForBook/TextProcessingForBook/Program.cs
@@ -6,6 +6,7 @@ using TextProcessingForBook.CustomText.TextContent;
 using TextProcessingForBook.CustomText.TextContent.Characters;
 using System.Xml.Linq;
 using System.Collections.Generic;
+using System.Text;
 
 namespace TextProcessingForBook
 {
@@ -13,81 +14,56 @@ namespace TextProcessingForBook
     {
         static void Main(string[] args)
         {
-            //Text zenText;
-            //var filePath = ConfigurationManager.AppSettings["FilePath"];
-            //using (var textFile = File.OpenText(filePath))
-            //{
-            //    try
-            //    {
-            //        zenText = new Text(textFile);
-            //    }
-            //    finally
-            //    {
-            //        if (textFile != null)
-            //            ((IDisposable)textFile).Dispose();
-            //    }
-            //}
-            //zenText.Display();
-            //Console.ReadKey();
-            //Console.Clear();
+            Text zenText;
+            var filePath = ConfigurationManager.AppSettings["FilePath"];
+            using (var textFile = File.OpenText(filePath))
+            {
+                try
+                {
+                    zenText = new Text(textFile);
+                }
+                finally
+                {
+                    if (textFile != null)
+                        ((IDisposable)textFile).Dispose();
+                }
+            }
+            zenText.Display();
+            Console.ReadKey();
+            Console.Clear();
 
-            //var newText1 = TextProcessing.OrderByAscendingCountWordsInSentence(zenText);
-            //newText1.Display();
-            //Console.ReadKey();
-            //Console.Clear();
+            var newText1 = TextProcessing.OrderByAscendingCountWordsInSentence(zenText);
+            newText1.Display();
+            Console.ReadKey();
+            Console.Clear();
 
-            //int lengthWord = 3;
-            //var listWords = TextProcessing.FindAllWordByLength(
-            //        TextProcessing.FindAllSentenceType(zenText, SentenceType.Interrogative),
-            //        lengthWord,
-            //        true);
-            //Console.WriteLine(string.Join(", ", listWords));
-            //Console.ReadKey();
-            //Console.Clear();
+            int lengthWord = 3;
+            var listWords = TextProcessing.FindAllWordByLength(
+                    TextProcessing.FindAllSentenceType(zenText, SentenceType.Interrogative),
+                    lengthWord,
+                    true);
+            Console.WriteLine(string.Join(", ", listWords));
+            Console.ReadKey();
+            Console.Clear();
 
-            //int index = 0;
-            //var newText2 = TextProcessing.DeleteWordsContainingCharacterType(
-            //        zenText,
-            //        CharacterType.Consonant,
-            //        index,
-            //        TextProcessing.FindAllWordByLength(zenText, lengthWord));
-            //newText2.Display();
-            //Console.ReadKey();
-            //Console.Clear();
+            int index = 0;
+            var newText2 = TextProcessing.DeleteWordsContainingCharacterType(
+                    zenText,
+                    CharacterType.Consonant,
+                    index,
+                    TextProcessing.FindAllWordByLength(zenText, lengthWord));
+            newText2.Display();
+            Console.ReadKey();
+            Console.Clear();
 
-            //var newText3 = TextProcessing.ReplaceWord(zenText, TextProcessing.FindAllWordByLength(zenText, lengthWord), "ERROR");
-            //newText3.Display();
+            var newText3 = TextProcessing.ReplaceWord(zenText, TextProcessing.FindAllWordByLength(zenText, lengthWord), "ERROR");
+            newText3.Display();
+            Console.ReadKey();
+            Console.Clear();
 
-            CreateObjectModel();
-        }
-
-        public static void CreateObjectModel()
-        {
-            XDocument xDocument =
-                new XDocument(
-                    new XElement("TextProcessingForBook", new XAttribute("type", "namespace"),
-                        new XElement("App", new XAttribute("type", "config")),
-                        new XElement("Program", new XAttribute("type", "main")),                        
-
-                        new XElement("CustomText", new XAttribute("type", "folder"),
-                            new XElement("IText", new XAttribute("type", "interface")),
-                            new XElement("Text", new XAttribute("type", "class")),
-                            new XElement("TextProcessing", new XAttribute("type", "class")),
-
-                            new XElement("TextContent", new XAttribute("type", "folder"),
-                                new XElement("AuxiliaryForSentence", new XAttribute("type", "static class")),
-                                new XElement("Sentence", new XAttribute("type", "class")),
-                                new XElement("SentenceType", new XAttribute("type", "enum")),
-                                new XElement("Word", new XAttribute("type", "class"))),
-
-                                new XElement("Characters", new XAttribute("type", "folder"),
-                                    new XElement("Character", new XAttribute("type", "class")),                                    
-                                    new XElement("CharacterHelper", new XAttribute("type", "static class")),
-                                    new XElement("CharacterType", new XAttribute("type", "enum"))))));
-
-            var filePath = ConfigurationManager.AppSettings["ObjectModel"];
-            xDocument.Save(filePath);
-            Console.WriteLine(xDocument.ToString());
-        }
+            var objectModel = ObjectModel.CreateObjectModel();
+            //ObjectModel.SaveObjectModel(objectModel, "ObjectModel", @"C:\Users\thedr\Desktop");
+            Console.WriteLine(objectModel);
+        }        
     }
 }
